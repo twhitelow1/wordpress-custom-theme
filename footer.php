758b4ec504790f22b26b4697af090fbacbdc47ed
Todd Whitelow <todd@whitelow.tech>
2021-01-17 01:08:02 -0700

    add menus

---
+++ footer.php
@@ -0,0 +1,13 @@
+    <!-- Bootstrap Javascript -->   
+    <?php
+      wp_footer();
+    ?>       
+<footer class="footer text-center py-2 theme-bg-dark">
+		   
+       <p class="copyright"><a href="https://youtube.com/FollowAndrew">FollowAndrew</a></p>
+  
+ </footer>
+
+</div>
+</body>
+</html> 
--- functions.php
+++ functions.php
@@ -1,5 +1,21 @@
 <?php
 
+function customtheme_theme_support(){
+  // Adds dynamic title tag support
+  add_theme_support('title_tag');
+}
+
+add_action('after_setup_theme', 'customtheme_theme_support');
+
+function customtheme_menus(){
+  $locations = array(
+      'primary' => "Desktop Primary Left Sidebar",
+      'footer' => "FooterMenu"
+  );
+  register_nav_menus($locations);
+}
+add_action('init', 'customtheme_menus');
+
 function customtheme_register_styles(){
   $version = wp_get_theme()->get( 'Version' );
   wp_enqueue_style('customtheme-bootstrap', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css",array(), '5.13.0/', 'all');
@@ -7,5 +23,13 @@ function customtheme_register_styles(){
   wp_enqueue_style('customtheme-styles', get_template_directory_uri()."/style.css",array(), $version, 'all');
 }
 add_action('wp_enqueue_scripts', 'customtheme_register_styles');
+
+function customtheme_register_scripts(){
+  wp_enqueue_script('customtheme-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js',array(), '3.4.1', true);
+  wp_enqueue_script('customtheme-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js',array(), '3.4.1', true);
+  wp_enqueue_script('customtheme-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js',array(), '3.4.1', true);
+  wp_enqueue_script('customtheme-main',get_template_directory_uri().'/assets/js.main.js',array(), '1.0', true);
+}
+add_action('wp_enqueue_scripts', 'customtheme_register_scripts');
 
 ?>
