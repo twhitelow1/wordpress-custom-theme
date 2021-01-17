f701bc5f477e35f24909bc342f27474a83aabe6b
Todd Whitelow <todd@whitelow.tech>
2021-01-17 01:34:46 -0700

    add featured images

--- functions.php
+++ functions.php
@@ -3,6 +3,8 @@
 function customtheme_theme_support(){
   // Adds dynamic title tag support
   add_theme_support('title_tag');
+  add_theme_support('custom-logo');
+  add_theme_support('post-thumbnails');
 }
 
 add_action('after_setup_theme', 'customtheme_theme_support');
--- header.php
+++ header.php
@@ -18,7 +18,7 @@
 <body>
     
     <header class="header text-center">	    
-	    <a class="site-title pt-lg-4 mb-0" href="index.html">SiteName.dev</a>
+	    <a class="site-title pt-lg-4 mb-0" href="index.html"><?php echo get_bloginfo('name'); ?></a>
         
 	    <nav class="navbar navbar-expand-lg navbar-dark" >
            
@@ -27,8 +27,13 @@
 			</button>
 
 			<div id="navigation" class="collapse navbar-collapse flex-column" >
-				<img class="mb-3 mx-auto logo" src="wp-content/themes/my-custom-theme/assets/images/logo.png" alt="logo" >		
-
+        <?php
+        if(function_exists('the_custom_logo')){
+          $custom_logo_id = get_theme_mod('custom_logo');
+          $logo = wp_get_attachment_image_src($custom_logo_id);
+       }
+        ?>
+        <img class="mb-3 mx-auto logo" src="<?php echo $logo[0] ?>" alt="logo">
         <?php
           wp_nav_menu(
             array(
