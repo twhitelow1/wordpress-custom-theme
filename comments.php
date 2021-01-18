6e9b76205e5b1b6aa8fc89d6d92851313cc3fef8
Todd Whitelow <todd@whitelow.tech>
2021-01-18 00:19:37 -0700

    add comments

--- comments.php
+++ comments.php
@@ -6,7 +6,7 @@
                   if( ! have_comments()){
                     echo "Leave a Comment";
                   }else {
-                    echo get_comments_count()." Comments";
+                    echo get_comments_number()." Comments";
                   }
                 ?>
                  </h2><!-- .comments-title -->
