80a0276750c29dd33c1339e7576437dd8f71473a
Todd Whitelow <todd@whitelow.tech>
2021-01-18 01:20:22 -0700

    add widgets

--- functions.php
+++ functions.php
@@ -34,4 +34,30 @@ function customtheme_register_scripts(){
 }
 add_action('wp_enqueue_scripts', 'customtheme_register_scripts');
 
+function customtheme_widget_areas(){
+  register_sidebar(
+    array(
+      'before_title' => '<h2>',
+      'after_title' => '</h2>',
+      'before_widget' => '<div>',
+      'after_widget' => '</div>',
+      'name' => 'Sidebar Area',
+      'id' => 'sidebar-1',
+      'description' => 'Sidebar Widget Area'
+    )
+  );
+    register_sidebar(
+      array(
+        'before_title' => '<h2>',
+        'after_title' => '</h2>',
+        'before_widget' => '',
+        'after_widget' => '',
+        'name' => 'Footer Area',
+        'id' => 'footer-1',
+        'description' => 'Footer Widget Area'
+      )
+    );
+}
+add_action('widgets_init', 'customtheme_widget_areas');
+
 ?>
